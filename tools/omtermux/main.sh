38300bd3b6dbba39ece11402d31e425a11174685
Toad114514 <xiaolan2332021@163.com>
2026-03-24 01:35:18 +0800

    update(omz): script/p10k install

--- tools/omtermux/main.sh
+++ tools/omtermux/main.sh
@@ -4,10 +4,13 @@ source $PREFIX/lib/tpaxs/global
 omt_path=$work_path/tools/omtermux
 source $omt_path/banner.sh
 
-ins_omz(){
+start_omz(){
   echo "正在安装 omz，请稍候..."
-  git clone https://github.com/oh-my-zsh/oh-my-zsh
-  cd oh-my-zsh
+  apt_echo zsh
+  # case $(read -p "使用国内源加速还是使用Github？[github/gitee]")
+  # git clone https://github.com/oh-my-zsh/oh-my-zsh
+  # cd oh-my-zsh
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 }
 
 check_omz(){
@@ -17,14 +20,34 @@ check_omz(){
     read -p "[y/n]: " sel
     case $sel in
       "y") start_omz ;;
-      "n") echo "goodbye" ;;
+      "n") exit 1 ;;
     esac
   fi
+}
+
+p10k_ins(){
+  case $(read -p "使用 Github 源还是 Gitee 源？（国内建议使用 Gitee 源，默认为 Gitee）[github/gitee]: ") in
+    "github") ROOT_DOMAIN="github.com" ;;
+    "gitee") ROOT_DOMAIN="gitee.com" ;;
+  esac
+  git clone https://${ROOT_DOMAIN}/romkatv/powerlevel10k --depth 1 $HOME
+  echo "source $HOME/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
+  echo "powerlevel10k配置完毕。接下来您需要重新打开 Termux 来继续配置主题。（等下脚本将帮助您退出 Termux，如果发现闪退请不要害怕，这是正常现象。"
+  sleep 3
+  kill -9 $(pgrep com.termux)
 }
 
 main(){
   printf "欢迎使用 oh-my-termux!\n"
   check_omz
+  echo " 1. 安装 Powerlevel10k 主题"
+  echo " 2. 安装命令自动补全插件"
+  echo " 3. 安装命令高光插件"
+  echo " 99. 退出"
+  case $(read -p "omz_install: ") in
+    "1") p10k_ins ;;
+    "99") exit 0 ;;
+  esac
 }
 
 banner
